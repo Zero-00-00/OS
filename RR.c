c730bdb202b2b09bd1b4e16aef03976c57d264b8
Zero_285 <praloymondal4@gmail.com>
2025-04-02 19:55:18 +0530

    Add files via upload

--- RR.c
+++ RR.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #define size 15
 
-int A[size][3];     //0->BT, 1->remaining time, 2->TAT
+int A[size][3];     // 0->BT, 1->remaining time, 2->TAT
 
 void RR(int A[size][3], int n, int q) {
-    int flag=1, TAT=0;
-    while(flag!=0) {
-        for(int i=0; i<n; i++) {
-            if (A[i][1]-q>0) {
-                A[i][1]-=q;
-                TAT+=q;
-                flag=1;
-            }
-            else if (A[i][1]!=0) {
-                TAT+=A[i][1];
-                A[i][2]=TAT;
-                A[i][1]=0;
-            }
-            else {
-                if (i!=n-1)
-                flag=0;
+    int flag = 1, TAT=0;
+    while (flag != 0) {
+        flag = 0;  // Reset flag to 0 (exit condition)
+        for (int i = 0; i < n; i++) {
+            if (A[i][1] > 0) {  // Process is still not complete
+                flag = 1;  // Set flag to 1 indicating at least one process is still running
+                if (A[i][1] > q) {
+                    A[i][1] -= q;
+                    TAT += q;  // Add the time quantum to TAT
+                } else {
+                    TAT += A[i][1];  // Add the remaining burst time to TAT
+                    A[i][2]=TAT;
+                    A[i][1] = 0;  // Mark this process as completed
+                }
             }
         }
     }
@@ -27,13 +25,13 @@ void RR(int A[size][3], int n, int q) {
 
 int main() {
     int n, q;
-    printf("Enter the number of process: ");
+    printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    for (int i=0; i<n; i++) {
-        printf("Enter the burst time of process %d: ", i+1);
+    for (int i = 0; i < n; i++) {
+        printf("Enter the burst time of process %d: ", i + 1);
         scanf("%d", &A[i][0]);
-        A[i][1]=A[i][0];
+        A[i][1] = A[i][0];  // Initialize remaining time to burst time
     }
 
     printf("Enter time quantum: ");
@@ -42,7 +40,10 @@ int main() {
     RR(A, n, q);
 
     printf("Process     Burst time      TAT      WT\n");
-    for (int j=0; j<n; j++) {
-        printf("P%d             %d            %d      %d\n", j+1, A[j][0], A[j][2], A[j][2]-A[j][0]);
+    for (int j = 0; j < n; j++) {
+        int WT = A[j][2] - A[j][0];  // Waiting time = Turnaround Time - Burst Time
+        printf("P%d             %d            %d      %d\n", j + 1, A[j][0], A[j][2], WT);
     }
+
+    return 0;
 }
--- SJF.c
+++ SJF.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define size 15
 
-int A[size][2];
+int A[size][2];  // A[i][0] = burst time, A[i][1] = process ID
 
 void swap(int* x, int* a, int* y, int* b) {
     int temp = *x;
@@ -14,24 +14,24 @@ void swap(int* x, int* a, int* y, int* b) {
 }
 
 int part(int A[size][2], int p, int r) {
-    int x = A[p][1];
-    int i = p;
-    for(int j = p+1; j < r+1; j++) {
-        if (A[j][0] >= x) {
-            i += 1;
-            swap (&A[i][0], &A[i][1], &A[j][0], &A[j][1]);
+    int x = A[r][0];  // Pivot is the burst time of the last element
+    int i = p - 1;
+
+    for(int j = p; j < r; j++) {
+        if (A[j][0] <= x) {  // Sort by burst time (ascending)
+            i++;
+            swap(&A[i][0], &A[i][1], &A[j][0], &A[j][1]);
         }
     }
-    swap(&A[i][0], &A[i][1], &A[p][0], &A[p][1]);            //swap the pivot with the element in A[i]
-    return i;
+    swap(&A[i + 1][0], &A[i + 1][1], &A[r][0], &A[r][1]);  // Move pivot to correct position
+    return i + 1;  // Return pivot index
 }
 
-int qsort(int A[size][2], int p, int r) {
-    int q;
+void qsort(int A[size][2], int p, int r) {
     if (p < r) {
-        q = part(A, p, r);
-        qsort(A, p, q-1);
-        qsort(A, q+1, r);
+        int q = part(A, p, r);
+        qsort(A, p, q - 1);
+        qsort(A, q + 1, r);
     }
 }
 
@@ -41,18 +41,21 @@ int main() {
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    for (int i=0; i<n; i++) {
-        printf("Enter the burst time of process %d: ", i+1);
+    for (int i = 0; i < n; i++) {
+        printf("Enter the burst time of process %d: ", i + 1);
         scanf("%d", &A[i][0]);
-        A[i][1]=i+1;
+        A[i][1] = i + 1;  // Assign process ID
     }
 
-    qsort(A, 0, n-1);
+    qsort(A, 0, n - 1);  // Sort based on burst time
 
-    int TAT=0;
+    int TAT = 0;  // Turnaround time
     printf("Process     Burst time      TAT      WT\n");
-    for (int j=0; j<n; j++) {
-        TAT+=A[j][0];
-        printf("P%d             %d            %d      %d\n", A[j][1], A[j][0], TAT, TAT-A[j][0]);
+    for (int j = 0; j < n; j++) {
+        TAT += A[j][0];  // Add burst time to Turnaround time
+        int WT = TAT - A[j][0];  // Waiting time = TAT - Burst time
+        printf("P%d             %d            %d      %d\n", A[j][1], A[j][0], TAT, WT);
     }
+
+    return 0;
 }
